581fc7ddfecbd0929e95840a926d4e5541f9adfe
Ubada-S <ubadasiddiqicoc@gmail.com>
2025-09-04 21:27:45 +0530

    Implement the first part of Hero section

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import React from "react";
 import tailwindcss from "@tailwindcss/vite";
+import React from "react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+  },
 });
